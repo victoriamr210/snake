ed645fef9d80254b57033292fb55b82059c6771e
victoriamr210 <victoria@DESKTOP-HQSIT74.localdomain>
2019-08-13 18:43:05 +0100

    Snake moves and eats

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 #include <SFML/Graphics.hpp>
 using namespace std;
 
+// g++ -c main.cpp
+// g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
+// ./sfml-app
+
+
 int main(int argc, char *argv[]) {
 
 	game A;
